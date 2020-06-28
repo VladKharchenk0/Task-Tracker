d5f1bc608e667c9ca7b334f340a5f0c6af66440c
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-06-28 12:30:14 +0300

    Created table tasks

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -2,10 +2,20 @@ create database TASK_TRACKER;
 
 create table users
 (
-    id         serial  PRIMARY KEY,
+    id         serial PRIMARY KEY,
     first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    email VARCHAR(50) UNIQUE,
-    password VARCHAR NOT NULL DEFAULT '123',
-    status VARCHAR(10)
+    last_name  VARCHAR(30),
+    email      VARCHAR(50) UNIQUE,
+    password   VARCHAR NOT NULL DEFAULT '123',
+    status     VARCHAR(10)
+);
+
+create table tasks
+(
+    id          serial PRIMARY KEY,
+    title       VARCHAR(30),
+    description text,
+    status      VARCHAR(11),
+    user_id     int,
+    foreign key (user_id) references users (id)
 );
